1b5d37156075fdb8150308aa56f965990f5c8ab8
fernando gregoris <fernandogregoris@gmail.com>
2015-10-21 22:51:20 -0300

    fix index

--- index.php
+++ index.php
@@ -1,233 +1,164 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>TITULO</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link href="css/style.css?1.0.0" rel="stylesheet">
-        <link href="css/responsive.css" rel="stylesheet">
-        <!-- Facebook Conversion Code for Visitas a Power60 -->
-    </head>
-    <body>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Heroic Features - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <!--FILA-->
-            <div class="row">
-                <div class="col-lg-6 col-md-6 ">
-
-                </div>
-
-                <div class="col-lg-6 col-md-6 ">
-                    <!--CONTADORES-->
-                    <div class="row count">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            <p class="count_title">OFERTA<br>LIMITADA!</p>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            <p class="count_box">
-                                <span id="hour"><?php echo date('h');?></span>:<span id="min"><?php echo date('i');?></span>:<span id="sec">20</span>
-                            </p>
-                        </div>
-                    </div>
-                    <!--CONTADORES-->
-                    <div id="form_box">
-                        <a name="seccion1"></a>
-                        <form role="form" method="post" action="thank-you.php">
-                            <div class="form-group">
-                                <input id="name" type="text" class="form-control" name="name" placeholder="Nombre">
-                                <span id="name-error"></span>
-                            </div>
-                            <div class="form-group">
-                                <input id="email" type="email" class="form-control" name="email" placeholder="E-mail">
-                                <span id="email-error"></span>
-                            </div>
-                            <div class="form-group">
-                                <input id="phone" type="tel" class="form-control" name="phone" maxlength="10" placeholder="Teléfono">
-                                <span id="phone-error"></span>
-                            </div>
-                            <input type="hidden" name="medio" value="<?php echo $medio; ?>">
-                            <button type="submit" class="btn btn-danger" id="form-botton">Más INFORMACIÓN!</button>
-                        </form>
-                        <p id="text1bb">haz click aquí para llamar ahora<br><a href="tel:18665010848" style="color: #203456; font-family: segoeuib;">1866-501-0848</a></p>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 ">
-
-                </div>             
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
             </div>
-            <!--FIN FILA-->            
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-        <script src="js/jquery-2.1.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.numeric.js"></script>
-        <!--CONTADORES-->    
-        <script>
-            function contador() {
-                var hour = parseInt($('#hour').text());
-                var min = parseInt($('#min').text());
-                var sec = parseInt($('#sec').text());
-                if (sec !== 00) {
-                    sec -= 1;
-                    if (sec < 10) {
-                        $('#sec').text('0' + sec);
-                    } else {
-                        $('#sec').text(sec);
-                    }
-                } else {
-                    $('#sec').text('59');
-                    if (min !== 00) {
-                        min -= 1;
-                        if (min < 10) {
-                            $('#min').text('0' + min);
-                        } else {
-                            $('#min').text(min);
-                        }
-                    } else {
-                        $('#min').text('59');
-                        hour -= 1;
-                        if (hour < 10) {
-                            $('#hour').text('0' + hour);
-                        } else {
-                            $('#hour').text(hour);
-                        }
-                    }
-                }
-            }
-            setInterval(contador, 1000);
-            function intermitente() {
-                var shadow = $('.count_title').css('textShadow');
-                if (shadow === 'rgb(255, 255, 0) 0px 0px 10px') {
-                    $('.count_title').css('textShadow', '0px 0px 0px #000');
-                } else {
-                    $('.count_title').css('textShadow', '0px 0px 10px #ff0');
-                }
-            }
-            setInterval(intermitente, 500);
-        </script>
-        <!--CONTADORES-->
-        <!--        INFO DISPOSITIVO-->
-        <script>
-//            $(function () {
-//                var resolucion = '';
-//                var plataforma = '';
-//                var sistema = '';
-//                //resolucion de pantalla
-//                resolucion = String(window.screen.width) + " x " + String(window.screen.height);
-//
-//                var isMobile = {
-//                    Android: function () {
-//                        return navigator.userAgent.match(/Android/i);
-//                    },
-//                    BlackBerry: function () {
-//                        return navigator.userAgent.match(/BlackBerry/i);
-//                    },
-//                    iOS: function () {
-//                        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//                    },
-//                    Opera: function () {
-//                        return navigator.userAgent.match(/Opera Mini/i);
-//                    },
-//                    Windows: function () {
-//                        return navigator.userAgent.match(/IEMobile/i);
-//                    },
-//                    any: function () {
-//                        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-//                    }
-//                };
-//                if (isMobile.any()) {
-//                    plataforma = 'Mobile';
-//                    if (isMobile.Android())
-//                        var sistema = 'Android';
-//                    if (isMobile.Windows())
-//                        var sistema = 'Windows phone';
-//                    if (isMobile.BlackBerry())
-//                        var sistema = 'BlackBerry';
-//                    if (isMobile.iOS())
-//                        var sistema = 'Iphone iOS';
-//                    if (isMobile.Opera())
-//                        var sistema = 'Opera';
-//                } else {
-//                    plataforma = 'Desktop';
-//                    //Check if browser is IE or not
-//                    if (navigator.userAgent.search("MSIE") >= 0) {
-//                        var sistema = "InternetExplorer";
-//                    }
-//                    //Check if browser is Chrome or not
-//                    else if (navigator.userAgent.search("Chrome") >= 0) {
-//                        var sistema = "Chrome";
-//                    }
-//                    //Check if browser is Firefox or not
-//                    else if (navigator.userAgent.search("Firefox") >= 0) {
-//                        var sistema = "FireFox";
-//                    }
-//                    //Check if browser is Safari or not
-//                    else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-//                        var sistema = "Safari";
-//                    }
-//                    //Check if browser is Opera or not
-//                    else if (navigator.userAgent.search("Opera") >= 0) {
-//                        var sistema = "Opera";
-//                    }
-//                }
-//                //alert(navigator.userAgent.split(")")[0]);
-//                alert('DISPOSITIVO: ' + navigator.userAgent.split(")")[0].substring(13));
-//                alert('RESOLUCION: ' + resolucion);
-//                alert('PLATAFORMA: ' + plataforma);
-//                alert('SISTEMA: ' + sistema);
-//            });
-        </script>
-        <!--        INFO DISPOSITIVO-->
-        <!--        VALIDACION DE FORMULARIO-->
-        <script>
-            $(function(){
-                $('#phone').numeric();
-                $('#phone').keyup(function(){
-                    if($('#phone').length === 1 && $('#phone').val() === '0'){
-                        $('#phone').val('');
-                    }
-                });
-            });
-            $("form").submit(function (event) {
-                if ($("#name").val() === "") {
-                    $("#name-error").text("Complete el campo");
-                    event.preventDefault();
-                } else {
-                    var text = $("#name").val();
-                    var acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç";
-                    var original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc";
-                    for (var i = 0; i < acentos.length; i++) {
-                        text = text.replace(acentos.charAt(i), original.charAt(i));
-                    }
-                    $("#name").val(text);
-                    $("#name-error").text("");
-                }
-                if ($("#email").val() === "" && $("#phone").val() === "") {
-                    $("#phone-error").text("Ingrese un numero valido");
-                    $("#email-error").text("Ingrese un e-mail valido");
-                    event.preventDefault();
-                }
-                if ($("#phone").val() !== "") {
-                    if ($("#phone").val().length !== 10 || isNaN($('#phone').val())) {
-                        $("#phone-error").text("Ingrese un numero de 10 digitos");
-                        event.preventDefault();
-                    } else {
-                        $("#phone-error").text("");
-                    }
-                }
-                if ($("#email").val() !== "") {
-                    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-                    if (!emailReg.test($("#email").val())) {
-                        $("#email-error").text("Ingrese un e-mail valido");
-                        event.preventDefault();
-                    } else {
-                        $("#email-error").text("");
-                    }
-                }
-            });
-        </script>
-        <!--        VALIDACION DE FORMULARIO-->
-        <script>
-            //Analytics
-        </script>
-    </body>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+            <p><a class="btn btn-primary btn-large">Call to action!</a>
+            </p>
+        </header>
+
+        <hr>
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Latest Features</h3>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Page Features -->
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Feature Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Feature Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Feature Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Feature Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
