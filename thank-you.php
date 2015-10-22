58f24c31c836929f02b32845c8ef950ffb25eb03
fernando gregoris <fernandogregoris@gmail.com>
2015-10-21 22:18:36 -0300

    repo add

---
+++ thank-you.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TITULO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style2.css" rel="stylesheet">
+        <link href="css/responsive2.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+
+                </div>
+                <div class="col-lg-6">         
+                </div>
+            </div>
+        </div>
+        <script src="js/jquery-2.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script>
+            //Analytics
+        </script>
+    </body>
+</html>
